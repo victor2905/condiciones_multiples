b005998d573886522558cab65f3fd7b2973a55c4
victor2905 <98538867+victor2905@users.noreply.github.com>
2023-01-27 22:17:32 -0500

    condicionales múltiples
    Creación de proyecto

---
+++ condicionales_multiples.py
@@ -0,0 +1,9 @@
+print("Condicionales múltiples")
+num_uno = 1
+if num_uno == 1:
+    print("El número es UNO.")
+elif num_uno == 2:
+    print("El número es DOS.")
+else:
+    print("El número se desconoce.")
+
